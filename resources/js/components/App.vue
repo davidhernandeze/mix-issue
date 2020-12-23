b57c562958e5f5d948450bd92799290904aa8991
David Hdz <david@xmlteam.com>
2020-12-22 23:57:52 -0600

    dashboard vue 3 installed

---
+++ resources/js/components/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        holka
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
