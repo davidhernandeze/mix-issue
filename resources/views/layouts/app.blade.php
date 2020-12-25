560898ae6d73b36ecb92719423ed40c22ff93291
David Hdz <davidhernandeze@gmail.com>
2020-12-25 12:13:06 -0600

    install ziggy for frontend routing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,10 +7,12 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Licit Action') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+
+    @routes
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
